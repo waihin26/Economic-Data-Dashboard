4905104b7a3256b5ef29e69d1c171ace2d779b5d
Wai Hin <138366109+waihin26@users.noreply.github.com>
2024-10-25 10:57:11 +0800

    Add requirement.txt file

--- data_fetcher/fred.py
+++ data_fetcher/fred.py
@@ -2,7 +2,7 @@ import pandas_datareader.data as web
 from datetime import datetime
 import pandas as pd
 
-def continued_claims(start_date, end_date):
+def continued_claims(start_date =None, end_date=None):
     if not start_date:
         start_date = '2015-01-01'
     if not end_date:
@@ -12,5 +12,4 @@ def continued_claims(start_date, end_date):
     df.columns = [name]
     return df
 
-start_date = '2015-01-01'
-end_date = datetime.now().strftime('%Y-%m-%d') 
+
